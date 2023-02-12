6a81265577631a8af7e18afb3b068c77c1fde6b6
abdurahmanjabiin <jabiin626@gmail.com>
2023-02-12 16:01:37 +0300

    responsive and adjusment

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const computerChoiceDisplay = document.getElementById('computer-choice')
 const userChoiceDisplay = document.getElementById('user-choice')
 const resultDisplay = document.getElementById('result')
+const pageOne = document.getElementById('pagestart')
+const startBtn = document.querySelector('.startbtn')
+const wrap = document.querySelector('.wrapper')
 const possibleChoices = document.querySelectorAll('.btn')
 let userChoice
 let computerChoice
@@ -12,6 +15,33 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
   getResult();
 }))
 
+
+
+function getResult() {
+  
+
+  if (computerChoice === 'rock' && userChoice === 'paper') {
+    resultDisplay.innerHTML = 'you win!'
+  }
+  if (computerChoice === 'rock' && userChoice === 'scissors') {
+    resultDisplay.innerHTML = 'you lost!'
+  }
+  if (computerChoice === 'paper' && userChoice === 'scissors') {
+    resultDisplay.innerHTML = 'you win!'
+  }
+  if (computerChoice === 'paper' && userChoice === 'rock') {
+    resultDisplay.innerHTML = 'you lose!'
+  }
+  if (computerChoice === 'scissors' && userChoice === 'rock') {
+    resultDisplay.innerHTML = 'you win!'
+  }
+  if (computerChoice === 'scissors' && userChoice === 'paper') {
+    resultDisplay.innerHTML = 'you lose!'
+  }
+  if (computerChoice === userChoice) {
+    resultDisplay.innerHTML = 'its draw!'
+  }
+}
 function generateComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1
   
@@ -27,29 +57,7 @@ function generateComputerChoice() {
   computerChoiceDisplay.innerHTML = computerChoice
 }
 
-function getResult() {
-  
-
-  if (computerChoice === 'rock' && userChoice === "paper") {
-    result = 'you win!'
-  }
-  if (computerChoice === 'rock' && userChoice === "scissors") {
-    result = 'you lost!'
-  }
-  if (computerChoice === 'paper' && userChoice === "scissors") {
-    result = 'you win!'
-  }
-  if (computerChoice === 'paper' && userChoice === "rock") {
-    result = 'you lose!'
-  }
-  if (computerChoice === 'scissors' && userChoice === "rock") {
-    result = 'you win!'
-  }
-  if (computerChoice === 'scissors' && userChoice === "paper") {
-    result = 'you lose!'
-  }
-  if (computerChoice === userChoice) {
-    result = 'its a draw!'
-  }
-  resultDisplay.innerHTML = result
-}
+startBtn.addEventListener('click', () => {
+  pageOne.style.display = 'none' 
+  wrap.style.display = 'flex'
+})
