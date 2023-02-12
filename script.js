9d137be546dec310f33d7ab27b8e1d3e73ed2fa2
abdurahmanjabiin <jabiin626@gmail.com>
2023-02-12 20:04:07 +0300

    fixed result bug

--- script.js
+++ script.js
@@ -1,60 +1,55 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const userChoiceDisplay = document.getElementById('user-choice')
-const resultDisplay = document.getElementById('result')
-const pageOne = document.getElementById('pagestart')
-const startBtn = document.querySelector('.startbtn')
-const wrap = document.querySelector('.wrapper')
-const possibleChoices = document.querySelectorAll('.btn')
-let userChoice
-let computerChoice
-let result
+let startBtn = document.getElementById('pagestart')
+let wrap = document.querySelector('.wrapper')
+let pageOne = document.getElementById('pagestart')
+let [computer_score,user_score]=[0,0];
+let result_ref = document.getElementById("result");
+let choices_object = {
+    'rock' : {
+        'rock' : 'draw',
+        'scissor' : 'win',
+        'paper' : 'lose'
+    },
+    'scissor' : {
+        'rock' : 'lose',
+        'scissor' : 'draw',
+        'paper' : 'win'
+    },
+    'paper' : {
+        'rock' : 'win',
+        'scissor' : 'lose',
+        'paper' : 'draw'
+    }
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
-  userChoice = e.target.id
-  generateComputerChoice();
-  getResult();
-}))
-
-
-
-function getResult() {
-  
-
-  if (computerChoice === 'rock' && userChoice === 'paper') {
-    resultDisplay.innerHTML = 'you win!'
-  }
-  if (computerChoice === 'rock' && userChoice === 'scissors') {
-    resultDisplay.innerHTML = 'you lost!'
-  }
-  if (computerChoice === 'paper' && userChoice === 'scissors') {
-    resultDisplay.innerHTML = 'you win!'
-  }
-  if (computerChoice === 'paper' && userChoice === 'rock') {
-    resultDisplay.innerHTML = 'you lose!'
-  }
-  if (computerChoice === 'scissors' && userChoice === 'rock') {
-    resultDisplay.innerHTML = 'you win!'
-  }
-  if (computerChoice === 'scissors' && userChoice === 'paper') {
-    resultDisplay.innerHTML = 'you lose!'
-  }
-  if (computerChoice === userChoice) {
-    resultDisplay.innerHTML = 'its draw!'
-  }
 }
-function generateComputerChoice() {
-  const randomNumber = Math.floor(Math.random() * 3) + 1
-  
-  if (randomNumber === 1) {
-    computerChoice = 'rock'
-  }
-  if (randomNumber === 2) {
-    computerChoice = 'scissors'
-  }
-  if (randomNumber === 3) {
-    computerChoice = 'paper'
-  }
-  computerChoiceDisplay.innerHTML = computerChoice
+
+function checker(input){
+    const choices = ["rock", "paper", "scissor"];
+    const num = Math.floor(Math.random()*3);
+
+    document.getElementById("comp_choice").innerHTML = 
+    ` Computer choose <span> ${choices[num].toUpperCase()} </span>`;
+
+    document.getElementById("user_choice").innerHTML = 
+    ` You choose <span> ${input.toUpperCase()} </span>`;
+
+    let computer_choice = choices[num];
+
+    switch(choices_object[input][computer_choice]){
+        case 'win':
+            result_ref.innerHTML = "YOU WIN";
+            user_score++;
+            break;
+        case 'lose':
+            result_ref.innerHTML = "YOU LOSE";
+            computer_score++;
+            break;
+        default:
+            result_ref.innerHTML = "DRAW";
+            break;
+    }
+
+    document.getElementById("computer_score").innerHTML = computer_score;
+    document.getElementById("user_score").innerHTML = user_score;
 }
 
 startBtn.addEventListener('click', () => {
